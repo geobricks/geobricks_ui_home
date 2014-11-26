2affb3bb5077f7faeb21ea665e4a1af80631714e
Kalimaha <guido.barbaglia@gmail.com>
2014-11-26 15:19:58 +0100

    multilanguage.

---
+++ nls/translate.js
@@ -0,0 +1,8 @@
+define({
+    root : {
+        title: 'Geobricks',
+        subtitle: 'Everything you need to acquire, process and distribute geospatial data. In one place.'
+    },
+    fr : true,
+    es : true
+});
