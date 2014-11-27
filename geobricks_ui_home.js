27741b82ad630a24b54d1e5b2888e3927f1c368b
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-11-27 11:10:26 +0100

    Root variable added.

--- geobricks_ui_home.js
+++ geobricks_ui_home.js
@@ -1,7 +1,9 @@
+var root = '../modules/';
+
 define(['jquery',
         'mustache',
-        'text!../modules/geobricks_ui_home/html/templates.html',
-        'i18n!../modules/geobricks_ui_home/nls/translate',
+        'text!' + root + 'geobricks_ui_home/html/templates.html',
+        'i18n!' + root + 'geobricks_ui_home/nls/translate',
         'bootstrap'], function ($, Mustache, templates, translate) {
 
     'use strict';
